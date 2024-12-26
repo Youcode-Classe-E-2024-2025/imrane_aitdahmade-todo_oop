10e222bb19b04a73e24b1bde52805976529d0d29
imrane-ait-dahmade <imraneaitdahmade2006@gmail.com>
2024-12-26 16:50:43 +0100

    creation des tableau SQL taches et users

--- db.sql
+++ db.sql
@@ -6,14 +6,17 @@ CREATE TABLE TACHES (
     id int PRIMARY KEY UNIQUE AUTO_INCREMENT,
     titre VARCHAR(255) UNIQUE NOT NULL,
     descreption TEXT  NOT NULL,
-    Attribué  VARCHAR(255) Foreign Key  REFERENCES USER(nom),
-    status VARCHAR(10) ENUM('À faire','En cours','Terminé') DEFAULT  'À faire',
-    type VARCHAR(10) ENUM('basique','bug','feature') DEFAULT  'basique',
+    Attribue  VARCHAR(255),
+    status  ENUM('À faire','En cours','Terminé') DEFAULT  'À faire',
+    type  ENUM('basique','bug','feature') DEFAULT  'basique',
+     Foreign Key (Attribue )  REFERENCES USER(nom)
 
 );
 CREATE TABLE USER (
   id int UNIQUE AUTO_INCREMENT,
   nom VARCHAR(255) PRIMARY KEY UNIQUE NOT NULL,
-  role VARCHAR(255) ENUM('admin','user') DEFAULT 'user',
-)
+  role  ENUM('admin','user') DEFAULT 'user'
+);
+
+
 
